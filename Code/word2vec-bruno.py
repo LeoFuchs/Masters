e07724bcb700766dc0d2b12ec84bef94066b965f
leofuchs <fuchs_leo@hotmail.com>
2019-02-13 10:59:37 -0300

    New Organization

---
+++ Code/word2vec-bruno.py
@@ -0,0 +1,121 @@
+# encoding: utf-8
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.datasets import load_files
+import gensim
+import matplotlib.pyplot as plt
+import math
+
+number_topics = 2
+number_words = 5
+max_document_frequency = 1.0
+min_document_frequency = 0.4
+ngram = (1, 3)
+max_features = None
+
+alpha = None
+beta = None
+learning = 'batch'  # Bacth ou Online
+iterations = 5000
+
+# Imprime os tópicos com as palavras em ordem
+def print_top_words(model, feature_names, number_words):
+
+    wiki = gensim.models.KeyedVectors.load_word2vec_format('/home/fuchs/Documentos/MESTRADO/Datasets/wiki-news-300d-1M.vec')
+
+    for topic_index, topic in enumerate(model.components_):
+        message = "Topic %d: " % (topic_index + 1)
+        message += "{"
+        message += " - ".join([feature_names[i] for i in topic.argsort()[:-number_words - 1:-1]])
+        message += "}\n"
+
+        print(message)
+
+        top_words = [feature_names[i] for i in topic.argsort()[:-number_words - 1:-1]]
+        for i in top_words:
+          #similar = wiki.most_similar(positive = i, topn = 5)
+          print("Most Similar Words for: %s" % i)
+          print(wiki.most_similar(positive = i, topn=5))
+
+
+def print_string_no_improvement(model, feature_names, number_words):
+  print("String without improvement:\n")
+  message = ("TITLE-ABS-KEY(")
+  for topic_index, topic in enumerate(model.components_):
+    message += "(\""
+    message += "\" AND \"".join([feature_names[i]
+                                 for i in topic.argsort()[:-number_words - 1:-1]])
+    message += "\")"
+    if topic_index < number_topics - 1:
+      message += " OR "
+    else:
+      message += ""
+  message += ")"
+  print(message)
+
+def print_string_with_improvement(model, feature_names, number_words):
+  print("String with improvement:\n")
+
+  #wiki = gensim.models.KeyedVectors.load_word2vec_format('/home/fuchs/Documentos/MESTRADO/Datasets/wiki-news-300d-1M.vec')
+
+  teste = [('Data', 0.7266719341278076), ('datasets', 0.7077047228813171), ('dataset', 0.6963621377944946), ('statistics', 0.6708579659461975), ('data--', 0.6617765426635742)]
+  message = ("TITLE-ABS-KEY(")
+  for topic_index, topic in enumerate(model.components_):
+    message += "(\""
+    message += "\" AND \"".join([feature_names[i]
+                                 for i in topic.argsort()[:-number_words - 1:-1]])
+    message += "\")"
+    if topic_index < number_topics - 1:
+      message += " OR "
+    else:
+      message += ""
+  message += ")"
+  print(message)
+
+
+
+
+
+# Carrega o dataset de treinamento
+files = load_files(container_path = '/home/fuchs/Documentos/MESTRADO/Masters/Files-QGS/QGS-ia-txt/complete', encoding="iso-8859-1")
+
+# Extrai as palavras e vetoriza o dataset
+tf_vectorizer = CountVectorizer(max_df = max_document_frequency,
+                                min_df = min_document_frequency,
+                                ngram_range = ngram,
+                                max_features = max_features,
+                                stop_words = 'english')
+
+tf = tf_vectorizer.fit_transform(files.data)
+
+# Salva os nomes das palavras em um dicionário
+dic = tf_vectorizer.get_feature_names()
+
+# Executa o LDA e treina-o
+lda = LatentDirichletAllocation(n_components = number_topics,
+                                doc_topic_prior = alpha,
+                                topic_word_prior = beta,
+                                learning_method = learning,
+                                learning_decay = 0.7,
+                                learning_offset = 10.0,
+                                max_iter = iterations,
+                                batch_size = 128,
+                                evaluate_every = -1,
+                                total_samples = 1000000.0,
+                                perp_tol = 0.1,
+                                mean_change_tol = 0.001,
+                                max_doc_update_iter = 100,
+                                random_state = None)
+lda.fit(tf)
+
+# Imprime os (number_topics) tópicos com as (number_words) palavras
+print("The %d topics with your %d words in textual format: \n" % (number_topics, number_words))
+
+model = gensim.models.KeyedVectors.load_word2vec_format('/home/fuchs/Documentos/MESTRADO/Datasets/wiki-news-300d-1M.vec')
+
+print_top_words(lda, dic, number_words)
+
+#model = gensim.models.KeyedVectors.load_word2vec_format('/home/fuchs/Documentos/MESTRADO/Datasets/wiki-news-300d-1M.vec')
+#similar = model.most_similar(positive=['man'], topn = 5)
+
+#print (similar)
